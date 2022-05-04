0237e7c1f5f5d619676d3185baea136e9fa94aa8
CKiumi <c.kiumi.oval17@gmail.com>
2022-05-05 00:30:45 +0900

    added katex in dev

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import "./style.css";
-
+import katex from "katex";
+import "katex/dist/katex.min.css";
 const app = document.querySelector<HTMLDivElement>("#app");
 if (app) {
   app.innerHTML = "<h1>Euler Tex</h1>";
+  katex.render("c = \\pm\\sqrt{a^2 + b^2}", app, {
+    throwOnError: false,
+  });
 }
