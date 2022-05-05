f61b68a12cbab82ee28036c1a3506c745b14b0c7
CKiumi <c.kiumi.oval17@gmail.com>
2022-05-05 13:30:27 +0900

    add main page

---
+++ index.ts
@@ -0,0 +1,20 @@
+import katex from "katex";
+import "katex/dist/katex.min.css";
+const main = () => {
+  render("Symbols", "abcdefghijk\\int");
+  render("Accent", "\\hat{x} \\tilde{y} \\tilde{K} \\hat{\\int}");
+  render("Square Root", "\\sqrt{a} \\sqrt{K} \\sqrt{\\int} ");
+  render("Left Right Parentheses", "\\left(x+y\\right) \\left(\\int+y\\right)");
+  render("Superscript Subscript", "x^a f^G K_s s_f x_a^b \\sum_x^y \\int_x^y");
+};
+const render = (title: string, latex: string) => {
+  const main = document.getElementById("main");
+  const h1 = document.createElement("h1");
+  h1.innerText = title;
+  const line = document.createElement("span");
+  line.classList.add("ruler");
+  main.append(h1, line);
+  katex.render(latex, line, { displayMode: true });
+};
+
+main();
---
+++ src/lib.ts
@@ -0,0 +1,7 @@
+export const tests = () => {
+  const span = document.createElement("span");
+  span.innerText = "F";
+  return span;
+};
+
+tests();
