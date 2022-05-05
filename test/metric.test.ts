869eeddf8f77c0fa6bc80972fe697c7bcffcdb9c
CKiumi <c.kiumi.oval17@gmail.com>
2022-05-05 18:08:58 +0900

    add getCharMetric

---
+++ test/metric.test.ts
@@ -0,0 +1,16 @@
+import { getCharMetrics } from "../font/index";
+import { expect, test } from "vitest";
+
+test("getCharMetrics", () => {
+  expect(() => {
+    getCharMetrics("∫", "Math-I");
+  }).toThrow("Font metrics not found for font: Math-I.");
+
+  expect(getCharMetrics("a", "Math-I")).toMatchObject({
+    depth: 0,
+    height: 0.43056,
+    italic: 0,
+    skew: 0,
+    width: 0.52859,
+  });
+});
