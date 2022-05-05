dc65e9df123360c4401dfeffdee8712eb8184176
CKiumi <c.kiumi.oval17@gmail.com>
2022-05-05 22:06:44 +0900

    add playwright

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 /// <reference types="vitest" />
 import { defineConfig, UserConfigExport } from "vite";
+import { configDefaults } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
   test: {
     environment: "happy-dom",
+    exclude: [...configDefaults.exclude, "test/e2e/*"],
   },
   alias: {
     src: path.resolve("src/"),
