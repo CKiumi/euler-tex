4ab3f3ba67458d4ef712ff7d8aab4bfe8233ba36
CKiumi <c.kiumi.oval17@gmail.com>
2022-07-16 13:37:13 +0900

    add display mode

--- src/lib.ts
+++ src/lib.ts
@@ -8,6 +8,12 @@ export * from "./parser/parser";
 export const latexToHtml = (latex: string) =>
   new GroupAtom(parse(latex)).toBox().toHtml();
 
+export const displayToHtml = (latex: string) => {
+  const inner = new GroupAtom(parse(latex), false).toBox().toHtml();
+  inner.className = "display";
+  return inner;
+};
+
 export const latexToEditableAtom = (latex: string) => {
   const atom = new GroupAtom(parse(latex, true), true);
   atom.toBox().toHtml();
